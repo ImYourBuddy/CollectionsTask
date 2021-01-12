9f5b5e1c8c86fa13df97f78a2f9ea5c14c0b9479
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-01-12 23:08:40 +0300

    Update ArrayListAndLinkedList.java

--- comparecollections/ArrayListAndLinkedList.java
+++ comparecollections/ArrayListAndLinkedList.java
@@ -4,13 +4,12 @@ import com.mycompany.firsttask.shapes.Circle;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ArrayListAndLinkedList {
     public static void main(String[] args) {
         ArrayList<Circle> arrayList = new ArrayList<>();
         LinkedList<Circle> linkedList = new LinkedList<>();
-        long startOperation;
-        long endOperation;
         for (int i = 0; i < 100000; i++) {
             if (i >= 0 && i < 20000) {
                 arrayList.add(new Circle(i + 1, "black"));
@@ -35,98 +34,121 @@ public class ArrayListAndLinkedList {
         }
 
         System.out.println("Add first.");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.add(0, new Circle(10.5, "purple"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
-
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.add(0, new Circle(10.5, "purple"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        addTest(arrayList, 0);
+        addTest(linkedList, 0);
         System.out.println();
 
         System.out.println("Add last.");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.add(arrayList.size(), new Circle(0.5, "brown"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
-
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.add(linkedList.size(), new Circle(0.5, "brown"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        addTest(arrayList, arrayList.size());
+        addTest(linkedList, linkedList.size());
         System.out.println();
 
         System.out.println("Add in the middle");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.add(arrayList.size() / 2, new Circle(5.0, "orange"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
-
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.add(linkedList.size() / 2, new Circle(5.0, "orange"));
-        }
-        endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        addTest(arrayList, arrayList.size() / 2);
+        addTest(linkedList, linkedList.size() / 2);
         System.out.println();
 
-        System.out.println("Search by index");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.get(arrayList.size() / 2);
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
+        System.out.println("Search by index from start");
+        searchByIndexTest(arrayList, 0);
+        searchByIndexTest(arrayList, 0);
+        System.out.println();
 
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.get(linkedList.size() / 2);
-        }
-        endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        System.out.println("Search by index from end");
+        searchByIndexTest(arrayList, arrayList.size());
+        searchByIndexTest(linkedList, linkedList.size());
+        System.out.println();
+
+        System.out.println("Search by index from middle");
+        searchByIndexTest(arrayList, arrayList.size() / 2);
+        searchByIndexTest(linkedList, linkedList.size() / 2);
         System.out.println();
 
         System.out.println("Search by value");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.indexOf("test addLast");
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
-
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.indexOf("test addLast");
-        }
-        endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        searchByValueTest(arrayList, new Circle(0.5, "purple"));
+        searchByValueTest(linkedList, new Circle(0.5, "purple"));
         System.out.println();
 
-        System.out.println("Delete");
-        startOperation = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            arrayList.remove(arrayList.size() / 2);
-        }
-        endOperation = System.nanoTime();
-        System.out.println("ArrayList: " + (endOperation - startOperation));
+        System.out.println("Delete from start");
+        deleteTest(arrayList, 0);
+        deleteTest(linkedList, 0);
+        System.out.println();
 
+        System.out.println("Delete from end");
+        deleteTest(arrayList, arrayList.size());
+        deleteTest(linkedList, linkedList.size());
+        System.out.println();
+
+        System.out.println("Delete from end");
+        deleteTest(arrayList, arrayList.size() / 2);
+        deleteTest(linkedList, linkedList.size() / 2);
+
+    }
+
+    public static void addTest(List list, int index) {
+        long startOperation = 0;
+        long endOperation = 0;
+        if (list.size() == index) {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.add(list.size(), new Circle(0.5, "purple"));
+            }
+            endOperation = System.nanoTime();
+        } else {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.add(index, new Circle(0.5, "purple"));
+            }
+            endOperation = System.nanoTime();
+        }
+        System.out.println(list.getClass().getSimpleName() + ": " + (endOperation - startOperation));
+    }
+
+    public static void searchByIndexTest(List list, int index) {
+        long startOperation = 0;
+        long endOperation = 0;
+        if (list.size() == index) {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.get(list.size() - 1);
+            }
+            endOperation = System.nanoTime();
+        } else {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.get(index);
+            }
+            endOperation = System.nanoTime();
+        }
+        System.out.println(list.getClass().getSimpleName() + ": " + (endOperation - startOperation));
+    }
+
+    public static <E> void searchByValueTest(List<E> list, E value) {
+        long startOperation = 0;
+        long endOperation = 0;
         startOperation = System.nanoTime();
         for (int i = 0; i < 10000; i++) {
-            linkedList.remove(linkedList.size() / 2);
+            list.indexOf(value);
         }
         endOperation = System.nanoTime();
-        System.out.println("LinkedList: " + (endOperation - startOperation));
+        System.out.println(list.getClass().getSimpleName() + ": " + (endOperation - startOperation));
+    }
+
+    public static void deleteTest(List list, int index) {
+        long startOperation = 0;
+        long endOperation = 0;
+        if (list.size() == index) {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.remove(list.size() - 1);
+            }
+            endOperation = System.nanoTime();
+        } else {
+            startOperation = System.nanoTime();
+            for (int i = 0; i < 10000; i++) {
+                list.remove(index);
+            }
+            endOperation = System.nanoTime();
+        }
+        System.out.println(list.getClass().getSimpleName() + ": " + (endOperation - startOperation));
     }
 }
